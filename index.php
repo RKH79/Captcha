3199ee73fcf6ac4ddfdb2922d9c401c41a205c0a
RKH79 <56217891+RKH79@users.noreply.github.com>
2020-08-08 23:58:15 +0430

    upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Captcha</title>
+</head>
+<body>
+<main>
+    <img src="Captcha.php" class="refresh-captcha"  alt="Captcha"/>
+    <script>
+        let refreshButton = document.querySelector(".refresh-captcha");
+        refreshButton.onclick = function() {
+            refreshButton.src = 'Captcha.php?' + Date.now();
+        }
+    </script>
+    <?php
+        echo password_hash("123456", PASSWORD_DEFAULT);
+        ?>
+</main>
+</body>
+</html>
